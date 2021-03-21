1e6f7bd3545714199b12a4f8f69fa2c82cc2df0b
ensarfurkanpasa@gmail.com <ensarfurkanpasa@gmail.com>
2021-03-21 15:39:02 +0300

    Add project files.

---
+++ Business/Abstract/IRentalService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IRentalService
+    {
+        IDataResult<List<RentalDetailDto>> GetRentalDetails();
+        IDataResult<Rental> GetByID(int ID);
+        IDataResult<List<Rental>> GetByCarID(int ID);
+        IDataResult<List<Rental>> GetByCustomerID(int ID);
+        IDataResult<List<Rental>> GetByRentDate(DateTime rentdate);
+        IDataResult<List<Rental>> GetByReturnDate(DateTime returndate);
+        IDataResult<List<Rental>> GetAll();
+        IResult Add(Rental rental);
+        IResult Update(Rental rental);
+        IResult Delete(Rental rental);
+    }
+}
---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto : IDto
+    {
+        public int ID { get; set; }
+        public string CarName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+
+    }
+}
